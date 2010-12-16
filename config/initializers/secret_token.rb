18b4f183e69841044cf41f3da55f31c5dc7b900d
gregor <gregor.swatek@gmail.com>
2010-12-16 20:54:03 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArasoDe::Application.config.secret_token = 'c12fa1e93f9eb75f7236bbf9118988bd7dec42e70a37220cde91918305e9a12daa3937b5df83c9a28f4ab2940d50998c8ba2b5f70b1923e74a4e2cf9d14d0221'
