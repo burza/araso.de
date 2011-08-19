988c6249f5049dca5560f51224a52fcb6e25c410
Patrick Bartels <patrick.bartels@lb-lab.de>
2011-08-19 12:19:38 +0200

    Changed to assembla

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :user, 'vu2030'
 set :group, 'www-data'
 
 set :repository, "git@github.com:burza/#{application}.git"
+set :repository, "git@git.assembla.com:araso.git"
 
 set :rails_root, "#{File.expand_path(File.dirname(__FILE__)+ '/..')}"
 
