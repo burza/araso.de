c8eb0ba6bad8a8b12bbc82287feba7696e344398
Gregor <gregor.swatek@gmail.com>
2011-09-07 18:48:22 +0200

    fixed contact form

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,10 +1,10 @@
 class Notifier < ActionMailer::Base
   default :from => "gregor.swatek@araso.de"
   
-  def contact_us(sender)
-    @sender = sender
+  def contact_us(contact)
+    @contact = contact
     mail(:to => "gregor.swatek@gmail.com",
-         :from => sender.email,
-         :subject => sender.subject)
+         :from => contact.email,
+         :subject => contact.subject)
   end
 end
