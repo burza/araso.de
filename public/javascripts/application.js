ecccb92b1f9c695d038d8adbd0bebb2bae37dabe
Gregor <gregor.swatek@gmail.com>
2011-09-13 18:02:53 +0200

    fixed contact form, fixed styles for fluid layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function(){
+  $('#flash_notice').delay(5000).fadeOut('slow');
+  $('#flash_error').delay(5000).fadeOut('slow');
 });
 
