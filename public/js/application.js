1cb0669f8df8785e8974e0c4b045b7264e8c572f
Gregor Swatek <gregor.swatek@gmail.com>
2013-07-21 01:52:16 +0200

    added fancybox for project images, fixed service images

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {  
   FastClick.attach(document.body);
+  
+  $(".fancybox")
+    .attr('rel', 'gallery_windows')
+    .fancybox({
+        padding : 0,
+        loop: true
+  });
+  $(".fancybox-if")
+    .attr('rel', 'gallery_if')
+    .fancybox({
+        padding : 0,
+        loop: true
+  });
+
+  $(".fancybox-dlt")
+    .attr('rel', 'gallery_dlt')
+    .fancybox({
+        padding : 0,
+        loop: true
+  });
 });
